6e7e26dc1cce22cc94d31e55467236b61f39b0d7
vozmak <strela996@bk.ru>
2021-08-25 16:31:43 +0300

    feat: Add POST request, user validation
    JIRA: MODULE-1-PART-3-GALLEREY

---
+++ authorization/authorization.js
@@ -0,0 +1,52 @@
+const form = document.getElementById("authorization")
+
+form.addEventListener("submit", async event => {
+  event.preventDefault()
+
+  const email = form.elements.email;
+  const password = form.elements.password;
+  const user = {
+    "email": email.value,
+    "password": password.value
+  };
+
+  let result = await authorizationUser(user);
+
+  if (result.errorMessage) {
+    email.value = "";
+    password.value = "";
+    return alert(result.errorMessage)
+  }
+
+  console.log(result.token);
+});
+
+async function authorizationUser(user) {
+  if (!userValidation(user)) {
+    const incorrect = document.querySelector(".incorrect");
+    incorrect.textContent = 'Некоректный ввод. Проверьте привильность email и пароля.';
+    setTimeout( () => {
+      incorrect.textContent = '';
+    }, 7000);
+    return {
+      "errorMessage": "Некоректный ввод. Проверьте привильность email и пароля."
+    };
+  }
+
+  let response = await fetch('https://glq7fjiy07.execute-api.us-east-1.amazonaws.com/api/login', {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json;charset=utf-8"
+    },
+    body: JSON.stringify(user)
+  });
+
+  return await response.json()
+}
+
+function userValidation({email, password}) {
+  const emailRegExp = /^[a-z\d]+@[a-z]+\.[a-z]+$/i;
+  const passRegExp = /^[a-z\d]{8}$/i;
+
+  return emailRegExp.test(email)? passRegExp.test(password) : false;
+}
